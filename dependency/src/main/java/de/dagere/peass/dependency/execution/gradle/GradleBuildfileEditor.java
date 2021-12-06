a55469eb1cc1080fe6720022f567029d7920992f
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-12-06 01:25:43 +0100

    Use generic separators

--- dependency/src/main/java/de/dagere/peass/dependency/execution/gradle/GradleBuildfileEditor.java
+++ dependency/src/main/java/de/dagere/peass/dependency/execution/gradle/GradleBuildfileEditor.java
@@ -129,7 +129,7 @@ public class GradleBuildfileEditor {
          if (visitor.getTestLine() != -1) {
             visitor.addLine(visitor.getTestLine() - 1, javaagentArgument);
          } else {
-            visitor.getLines().add("\ntest\n {\n" + javaagentArgument + "\n }");
+            visitor.getLines().add(System.lineSeparator() + "test" + System.lineSeparator() + "{" + System.lineSeparator() + javaagentArgument + System.lineSeparator() + " }");
          }
       }
    }
